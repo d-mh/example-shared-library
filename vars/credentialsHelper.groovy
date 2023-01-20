b56d55ce5cf9259032633aba3e99c7164eb0a3e5
Dariusz Mach <mach.dariusz@gmail.com>
2023-01-20 14:10:18 +0100

    add loadCredential()

--- vars/credentialsHelper.groovy
+++ vars/credentialsHelper.groovy
@@ -1,3 +1,17 @@
 def call() {
     sh 'echo Hello World from SL'
+
+    
+}
+
+def loadCredential() {
+    node {
+        environment {
+            TOKEN = credentials('secret1')
+        }
+        
+        stage('load credentials') {
+            sh "echo load credentials stage ${TOKEN}"
+        }
+    }
 }
